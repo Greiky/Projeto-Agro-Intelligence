f6fcb817dd5eb7636b2f9286d7fb7d307c994882
Greiky de Paula Gouveia <greiky.gouveia@gmail.com>
2026-08-12 21:48:08 -0300

    Adiciona analise de umidade por registro

--- 02_Python/analise_plantacao.py
+++ 02_Python/analise_plantacao.py
@@ -10,3 +10,46 @@ plantacao = pd.read_csv(arquivo)
 print("=== AGRO INTELLIGENCE ===")
 print("\nDados da plantação:")
 print(plantacao)
+
+# Calculando a temperatura média
+Temperatura_media = plantacao["Temperatura"].mean()
+
+print("\nTemperatura média da plantação:")
+print(Temperatura_media)
+
+# Calculando a umidade média do solo
+Umidade_media = plantacao["Umidade_Solo"].mean()
+
+print("\nUmidade média do solo:")
+print(Umidade_media)
+
+# Calculando o pH médio do solo
+ph_medio = plantacao["pH"].mean()
+
+print("\npH médio do solo:")
+print(ph_medio)
+
+if Umidade_media < 40:
+    print("Umidade baixa")
+
+elif Umidade_media > 70:
+    print("Umidade alta")
+
+# Analisando a umidade de cada dia
+for indice, linha in plantacao.iterrows():
+
+    umidade = linha["Umidade_Solo"]
+
+    print("\nData:", linha["Data"])
+    print("Umidade:", umidade)
+
+    if umidade < 40:
+        print("⚠️ ALERTA: Umidade baixa.")
+
+    elif umidade > 70:
+        print("⚠️ ALERTA: Umidade alta.")
+
+    else:
+        print("✅ Umidade dentro da faixa analisada.")
+
+
